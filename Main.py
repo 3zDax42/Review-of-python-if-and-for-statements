16ff6d11f48a62c773c934ebe839bc4baf7e7c92
3zDax42 <ezri@thebandit.org>
2025-01-07 14:52:12 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,20 @@
+# Asks the number of lines cleared in tetris nad prints out tiers acording to score
+tier = int(input("Enter the number of lines cleared in Tetris:"))
+if tier < 100:
+    print("Novice")
+elif tier < 200:
+    print("Intermediate")
+elif tier < 300:
+    print("Adcanced")
+else:
+    print("Expert")
+
+# Prints out numbers 2 - 80 counting by tens
+for i in range(2, 80, 10):
+    print(i, end = " ")
+print ("")
+
+# Prints out numbers 100 - 10 counting down by 5
+for i in range(100, 9, -5):
+    print(i, end = " ")
+print ("")
